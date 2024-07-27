2cf31807f764576a6cf8113b60ca514a523114f3
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-07-27 15:49:31 +0530

    songs features added

--- frontend/src/context/PlayerContext.jsx
+++ frontend/src/context/PlayerContext.jsx
@@ -37,6 +37,7 @@ export const PlayerContextProvider = (props) => {
     setTimeout(() => {
         
         audioRef.current.ontimeupdate = () => {
+            seekBar.current.style.width =(Math.floor(audioRef.current.currentTime / audioRef.current.duration*100)) + "%";
             setTime({
                 currentTime: {
                     second: Math.floor(audioRef.current.currentTime % 60),
