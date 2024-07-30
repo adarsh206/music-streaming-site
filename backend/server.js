789f04aca5b921afe989cc842a1344b3a464ef22
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-07-30 16:12:15 +0530

    routers added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import songRouter from './src/routes/songRoute.js';
 
 
 // app config
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 // initializing api routes
+app.use("/api/song", songRouter);
 app.get('/', (req, res) => res.send("API Working"));
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
--- backend/src/routes/songRoute.js
+++ backend/src/routes/songRoute.js
@@ -2,3 +2,8 @@ import { addSong, listSong } from "../controllers/songController.js";
 import express from 'express';
 
 const songRouter = express.Router();
+
+songRouter.post('/add', addSong);
+songRouter.get('/list', listSong);
+
+export default songRouter;
