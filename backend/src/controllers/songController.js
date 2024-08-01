ad5d819e8619e33296fd3effa002e7fd1ecf9236
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-08-01 08:11:06 +0530

    all songs features added

--- backend/src/controllers/songController.js
+++ backend/src/controllers/songController.js
@@ -34,6 +34,16 @@ const addSong = async (req,res) => {
 }
 
 const listSong = async (req,res) => {
+    try {
+      
+        const allSongs = await songModel.find({});
+        res.json({success:true, songs:allSongs})
+
+    } catch (error) {
+       
+        res.json({success:false});
+        
+    }
 
 }
 
