7985a95652818c1ce342c18e2ba44498e713970f
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-08-03 06:08:48 +0530

    react toastify added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
   return (
-    <div>
-      Admin
+    <div className='flex items-start min-h-screen'>
+      <ToastContainer />
     </div>
   )
 }
