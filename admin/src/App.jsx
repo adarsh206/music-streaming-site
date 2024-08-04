630f99ab53bc28b2a65a82523c8ea20abfc855e8
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-08-04 15:46:53 +0530

    react toastify added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
   return (
     <div className='flex items-start min-h-screen'>
       <ToastContainer />
+      <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
+          <div className='pt-8 pl-5 sm:pt-12 sm:pl-12'>
+
+          </div>
+      </div>
     </div>
   )
 }
