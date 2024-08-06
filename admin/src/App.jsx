c334585de99a7c6eaf463bfb715c4ba8a6473f57
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-08-06 09:09:21 +0530

    routes added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Routes, Route } from 'react-router-dom';
+import AddSong from './pages/AddSong';
+import AddAlbum from './pages/AddAlbum';
 
 
 const App = () => {
@@ -12,7 +14,8 @@ const App = () => {
       <div className='flex-1 h-screen overflow-y-scroll bg-[#F3FFF7]'>
           <div className='pt-8 pl-5 sm:pt-12 sm:pl-12'>
               <Routes>
-                <Route path='/add-song' element={} />
+                <Route path='/add-song' element={<AddSong />} />
+                <Route path='/add-album' element={<AddAlbum />} />
               </Routes>
           </div>
       </div>
